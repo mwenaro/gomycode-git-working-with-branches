5daca3f95c65d42c623593d29e8fad319bd407b7
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-04-28 19:43:50 +0300

    chore: update the s&b-nums.js. implemented all the functionality

--- small-and-biggest-nums.js
+++ small-and-biggest-nums.js
@@ -1,6 +1,6 @@
 // Create a function that takes an array of numbers
 //  and return both the minimum
-//  and maximum numbers, 
+//  and maximum numbers,
 // in that order.
 
 /*
@@ -11,3 +11,15 @@ minMax([2334454, 5]) ➞ [5, 2334454]
 minMax([1]) ➞ [1, 1]
 */
 
+// const minMax = (nums) => {
+//     const min = Math.min(...nums)
+//     const max = Math.max(...nums)
+//     return [min, max]
+// };
+
+const minMax = nums => [Math.min(...nums), Math.max(...nums)];
+console.log(minMax([1, 2, 3, 4, 5])) // [1,5]
+
+console.log(minMax([2334454, 5])) //➞ [5, 2334454]
+
+console.log(minMax([1])) //➞ [1, 1]
